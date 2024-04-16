c0168ba1d554813572a0a89c2717158003d7638b
0BIG-BANG0 <96254268+0BIG-BANG0@users.noreply.github.com>
2024-04-16 20:38:14 +0530

    Bolier Plate for Starting Node Project

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from "express"
+
+//Create an instance of express app
+const app = express()
+
+
+
+export default app
---
+++ server.js
@@ -0,0 +1,5 @@
+import app from "./index.js"
+
+app.listen(1100, () => {
+    console.log("App is listening to port 1100")
+})
