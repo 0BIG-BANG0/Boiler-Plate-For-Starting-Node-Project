fecc0ba99f4f850a509b46703761cf83f9ab6e47
0BIG-BANG0 <96254268+0BIG-BANG0@users.noreply.github.com>
2024-04-16 20:54:35 +0530

    boiler Plate with complete with ejs and expressLayout libraries

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 import express from "express"
+import path from "path"
+import expressLayout from "express-ejs-layouts"
 
 //Create an instance of express app
 const app = express()
 
+//for static file like css 
+app.use(express.static("views"));
 
+//Setting up the view Engine (ejs in this case)
+app.use("view engine", ejs)
+app.set("views", path.join(path.resolve(), "src","views"))
+
+//Using Express-ejs-Layout for Layout support
+app.use(expressLayout)
+
+//Serving static Files from 'src/views' directory
+app.use(express.static("src/views"))
 
 export default app
